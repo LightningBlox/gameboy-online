cfe1279f5b72559b31b87b4658a01c234320b4c8
Jeremy Banks <jeremy@jeremybanks.com>
2011-01-10 04:18:02 -0500

    Added res/proxy.php support to Python server. Not necessarily secure.

--- server.py
+++ server.py
@@ -1,12 +1,44 @@
 #!/usr/bin/env python
+import base64
 import socket
 import sys
 import webbrowser
-from SocketServer import TCPServer
+import urllib2
+import urlparse
+from SocketServer import TCPServer, ThreadingMixIn
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 
-class Handler(SimpleHTTPRequestHandler):
-    """TODO: Handle requests to res/proxy.php"""
+class Handler(ThreadingMixIn, SimpleHTTPRequestHandler):
+    """Extends SimpleHTTPRequestHandler to proxy at res/proxy.php."""
+    
+    """if ?url then grab it, passing user-agent, and echo it base64ed,
+    Content-Type: text/plain; Expries: in a day or whatever."""
+    
+    def do_GET(self):
+        path = urlparse.urlparse(self.path)
+        
+        if path.path == "/res/proxy.php":
+            qs = urlparse.parse_qs(path.query)
+            url = qs["url"][0].strip()
+            
+            self.log_message("Proxy Request: %s", url)
+            
+            try:
+                contents = urllib2.urlopen(url).read()
+            except urllib2.URLError:
+                self.log_error("Proxy - 404")
+                self.send_error(404)
+                self.end_headers()
+                return
+            
+            encoded = base64.b64encode(contents)
+            self.log_message("Proxy - 200 for %s", qs["url"][0])
+            self.send_response(200)
+            self.end_headers()
+            
+            self.wfile.write(encoded)
+        else:
+            return SimpleHTTPRequestHandler.do_GET(self)
 
 def main(argument=None):
     """Hosts the emulator on a free port and opens it in a web browser.
@@ -27,7 +59,7 @@ def main(argument=None):
                 raise e
     
     if argument not in set(("--dont-open", "-d")):
-        webbrowser.open("http://localhost:%s" + port)
+        webbrowser.open("http://localhost:%s" % port)
     
     server.serve_forever()
 
